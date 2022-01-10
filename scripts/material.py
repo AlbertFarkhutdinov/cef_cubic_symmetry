d7f18a5bde8e109b31d111188762a202bc7542f0
AlbertFarkhutdinov <farhutdinovalbert@yandex.ru>
2022-01-10 16:24:44 +0300

    added material.py

---
+++ scripts/material.py
@@ -0,0 +1,15 @@
+from scripts.common.tabular_information import Element
+from scripts.common.tabular_information import RARE_EARTHS, RARE_EARTHS_NAMES
+
+
+class Material:
+
+    def __init__(
+            self,
+            rare_earth_symbol: Element,
+            crystal: str = '?',
+    ):
+        self.rare_earth = RARE_EARTHS[
+            RARE_EARTHS_NAMES.index(rare_earth_symbol)
+        ]
+        self.crystal = crystal
